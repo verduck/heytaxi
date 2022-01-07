71093d4243171943f5d01913ba44c8106ecefa5e
cwooder <choijy8816@gmail.com>
2022-01-07 21:24:21 +0900

    initial commit

---
+++ src/main/java/com/moca/heytaxi/HeytaxiApplication.java
@@ -0,0 +1,13 @@
+package com.moca.heytaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeytaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeytaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moca/heytaxi/HeytaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moca.heytaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeytaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
