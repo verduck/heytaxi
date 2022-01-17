34ffdfa52e693c2acde21c9afb9a40b72f9c901e
cwooder <choijy8816@gmail.com>
2022-01-17 21:51:30 +0900

    WaitingRepository ZSet으로 변경

--- src/main/java/com/moca/heytaxi/service/CallService.java
+++ src/main/java/com/moca/heytaxi/service/CallService.java
@@ -1,6 +1,7 @@
 package com.moca.heytaxi.service;
 
 import com.moca.heytaxi.domain.Waiting;
+import com.moca.heytaxi.dto.CallDTO;
 import com.moca.heytaxi.repository.WaitingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,12 @@ public class CallService {
         this.waitingRepository = waitingRepository;
     }
 
+    public Waiting callTaxi(CallDTO call) {
+        return null;
+    }
+
     public Waiting waitCall(Waiting waiting) {
-        return waitingRepository.save(waiting);
+        waitingRepository.add(waiting);
+        return waiting;
     }
 }
