cc0100ca026b4487a5e57f927336872642fe1099
jeyog <choijy8816@gmail.com>
2022-03-20 17:32:06 +0900

    Websocket jwt 인증 stomp CONNECT일때만 수행

--- src/main/java/com/moca/heytaxi/security/JwtChannelInterceptor.java
+++ src/main/java/com/moca/heytaxi/security/JwtChannelInterceptor.java
@@ -5,6 +5,7 @@ import com.moca.heytaxi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,28 +17,31 @@ public class JwtChannelInterceptor implements ChannelInterceptor {
     private final JwtProvider jwtProvider;
     private final UserService userService;
 
-    @Override
-    public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        final String authorizationHeader = headerAccessor.getFirstNativeHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String token = authorizationHeader.substring(7);
-            Long userId = jwtProvider.getUserId(token);
-            User user = userService.loadUserById(userId);
-
-            if (jwtProvider.validateToken(token, user)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        user, null, user.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                headerAccessor.setUser(usernamePasswordAuthenticationToken);
-            }
-        }
-        return message;
-    }
-
     @Autowired
     public JwtChannelInterceptor(JwtProvider jwtProvider, UserService userService) {
         this.jwtProvider = jwtProvider;
         this.userService = userService;
+    }
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        final StompHeaderAccessor headerAccessor = StompHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (headerAccessor.getCommand() == StompCommand.CONNECT) {
+            final String authorizationHeader = headerAccessor.getFirstNativeHeader("Authorization");
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                String token = authorizationHeader.substring(7);
+                Long userId = jwtProvider.getUserId(token);
+                User user = userService.loadUserById(userId);
+
+                if (jwtProvider.validateToken(token, user)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                            user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                    headerAccessor.setUser(usernamePasswordAuthenticationToken);
+                }
+            }
+        }
+        return message;
     }
 }
